e5f48a5d0afac44248cec6499994353d03f91770
Junaid Babar <junaidbabar125@gmail.com>
2025-10-25 19:39:26 +0500

    feat: add TODO ERRORS  for testing pages in HomePage component

--- apps/client/app/page.tsx
+++ apps/client/app/page.tsx
@@ -1,5 +1,11 @@
 import { LandingPage } from "@/components/landing-page"
 
+
+TODO: 
+testing:
+  /people me pages ki
+  /warehouse ki
+
 export default function HomePage() {
   return <LandingPage />
 }
